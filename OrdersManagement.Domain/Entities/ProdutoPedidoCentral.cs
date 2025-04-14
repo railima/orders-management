e7a8a86fb2d8d53d3d7b11885555f0bd32c6d268
Rai Lima <raioliveira.ec@gmail.com>
2025-04-14 01:45:03 -0300

    Add data annotations for validation in ProdutoPedidoCentral and ProdutoPedidoCliente entities

--- OrdersManagement.Domain/Entities/ProdutoPedidoCentral.cs
+++ OrdersManagement.Domain/Entities/ProdutoPedidoCentral.cs
@@ -1,11 +1,18 @@
+using System.ComponentModel.DataAnnotations;
 using OrdersManagement.Domain.DTOs;
 
 namespace OrdersManagement.Domain.Entities
 {
     public class ProdutoPedidoCentral
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(100)]
         public required string NomeProduto { get; set; }
+        [Required]
         public int Quantidade { get; set; }
         public int PedidoCentralId { get; set; }
         public PedidoCentral? PedidoCentral { get; set; }
--- OrdersManagement.Domain/Entities/ProdutoPedidoCliente.cs
+++ OrdersManagement.Domain/Entities/ProdutoPedidoCliente.cs
@@ -1,11 +1,18 @@
+using System.ComponentModel.DataAnnotations;
 using OrdersManagement.Domain.DTOs;
 
 namespace OrdersManagement.Domain.Entities
 {
     public class ProdutoPedidoCliente
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(100)]
         public required string NomeProduto { get; set; }
+        [Required]
         public int Quantidade { get; set; }
         public int PedidoClienteId { get; set; }
         public PedidoCliente? PedidoCliente { get; set; }
